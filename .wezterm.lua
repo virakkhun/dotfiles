31b4608f85e79865bc060b2c60ee724e1b338db2
virakkhun <virak.dev.2000@gmail.com>
2025-10-08 23:12:41 +0700

    chore: copy wezterm config

---
+++ .wezterm.lua
@@ -0,0 +1,69 @@
+local wezterm = require("wezterm")
+
+local config = wezterm.config_builder()
+
+config.font_size = 19
+config.line_height = 1.0
+config.font = wezterm.font("BlexMono Nerd Font Mono")
+
+local function get_theme()
+	if wezterm.gui then
+		return wezterm.gui.get_appearance() == "Dark" and "tokyonight_night" or "tokyonight_day"
+	end
+
+	return "tokyonight_night"
+end
+
+config.color_scheme = get_theme()
+
+config.colors = {
+	cursor_bg = "#7aa2f7",
+	cursor_border = "#7aa2f7",
+}
+
+config.window_decorations = "RESIZE"
+config.enable_tab_bar = false
+
+config.keys = {
+	{
+		key = "d",
+		mods = "CMD",
+		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = "d",
+		mods = "CMD|SHIFT",
+		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = "c",
+		mods = "CMD",
+		action = wezterm.action.SendString("clear\n"),
+	},
+	{
+		key = "]",
+		mods = "CMD",
+		action = wezterm.action.ActivatePaneDirection("Right"),
+	},
+	{
+		key = "]",
+		mods = "CMD|SHIFT",
+		action = wezterm.action.ActivatePaneDirection("Down"),
+	},
+	{
+		key = "[",
+		mods = "CMD",
+		action = wezterm.action.ActivatePaneDirection("Left"),
+	},
+	{
+		key = "[",
+		mods = "CMD|SHIFT",
+		action = wezterm.action.ActivatePaneDirection("Up"),
+	},
+	{ key = "h", mods = "CMD", action = wezterm.action.SendKey({ key = "LeftArrow" }) },
+	{ key = "j", mods = "CMD", action = wezterm.action.SendKey({ key = "DownArrow" }) },
+	{ key = "k", mods = "CMD", action = wezterm.action.SendKey({ key = "UpArrow" }) },
+	{ key = "l", mods = "CMD", action = wezterm.action.SendKey({ key = "RightArrow" }) },
+}
+
+return config
--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -3,4 +3,13 @@ return {
     "mason-org/mason.nvim",
     opts = {},
   },
+  {
+    "mason-org/mason-lspconfig.nvim",
+    opts = {
+      ensure_installed = {
+        "lua_ls",
+        "vtsls",
+      },
+    },
+  },
 }
